987d9e20e956eda12b73821ee87a338113f21203
joud alnsour <97675816+joud-alnsour@users.noreply.github.com>
2022-02-06 23:03:25 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,123 +1,9 @@
 /* eslint-disable no-undef */
-
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const PORT = process.env.PORT;
-const app = express();
-const { default: axios } = require('axios');  
-const pg = require('pg');  
-app.use(express.json());
-app.use(cors());
-
-
-const client = new pg.Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
-});
-
-const movieData = require('./Movie Data/data.json');
-
-
-app.get('/',movieInfoHandler);
-app.get('/favorite',favHandler);
-app.get('/trending',trendingHandler);
-app.get('/search',searchHandler);
-app.get('/genre',genreHandler);
-app.get('/region',regionHandler);
-
-app.post('/addMovie',addMovieHandler);
-app.get('/getMovies',getMoviesHandler);
-
-
-app.put('/UPDATE/:id',updateidHandler);  
-app.delete('/DELETE/:id',deleteidHandler); 
-app.get('/getOneMoive/:id',getOneMovieHandler);
-
-
-app.get('*',notFoundHandler);  
-app.use(errorHandler); 
-
-
-const url =`https://api.themoviedb.org/3/trending/movie/day?api_key=${process.env.APIKEY}`;
-
-let urlSearchMovie = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.APIKEY}&language=en-US&query=Spider-Man&page=1&include_adult=false&year=2022`;
-let urlGenre = `https://api.themoviedb.org/3/genre/movie/list?api_key=${process.env.APIKEY}&language=en-US`;
-let urlRegion = `https://api.themoviedb.org/3/watch/providers/regions?api_key=${process.env.APIKEY}&language=en-US`;
-
-
-function Movie(title,poster_path,overview){
-  this.title = title;
-  this.posterPath = poster_path;
-  this.overview = overview;
-
-}
-function Trending (id,title,release_date,poster_path,overview){
-  this.id = id;
-  this.title = title;
-  this.releaseDate = release_date;
-  this.posterPath = poster_path;
-  this.overview = overview;
-
-}
-
-function Genre(id,name){
-  this.id = id;
-  this.name = name;
-
-}
-
-
-function Search (id,title,release_date,poster_path,overview){
-  this.id = id;
-  this.title = title;
-  this.releaseDate = release_date;
-  this.posterPath = poster_path;
-  this.overview = overview;
-
-}
-
-
-function Region(iso_3166_1,english_name,native_name){
-  this.iso_3166_1 = iso_3166_1;
-  this.english_name = english_name;
-  this.native_name = native_name;
-}
-
-
-function errorHandler(err, req, res) {
-  const errorr = {
-    status: 500,
-    message: 'error',
-  };
-  res.status(500).send(errorr);
-}
-
-
-function regionHandler(req,res){
-  axios.get(urlRegion)
-    .then((result)=>{
-      
-      let movieRegion = result.data.results.map(movie =>{
-        return new Region(movie.iso_3166_1,movie.english_name,movie.native_name);
-      });
-      res.status(200).json(movieRegion);
-
-    }).catch((err) =>{
-      errorHandler(err, req, res);
-
-    });
-}
-
-
-
-/* eslint-disable no-undef */
 'use strict';
- 
-require('dotenv').config();  
 
- 
-const express = require('express'); 
+  
+require('dotenv').config(); 
+const express = require('express');  
  
 const cors = require('cors');  
  
@@ -129,27 +15,26 @@ const PORT = process.env.PORT;
 const app = express();  
 app.use(cors());  
 
-app.use(express.json());  
- 
+app.use(express.json());   
+   
 
-const pg = require('pg'); 
+const pg = require('pg');  
 
  
 const client = new pg.Client({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
 });
-
  
 
 app.get('/', homePageHandler); 
-app.get('/Favorite', favouritePageHandler); 
+app.get('/Favorite', favouritePageHandler);  
 app.get('/trending', trendingPageHandler);
 app.get('/regions', regionsHandler);
 app.get('/search', searchHandler);
 app.get('/genre', genreHandler);
 
-app.post('/addmovie', addMovieHandler);  
+app.post('/addmovie', addMovieHandler); 
 app.get('/getmyMovies', getMoviesHandler);  
 
  
@@ -158,9 +43,7 @@ app.delete('/DELETE/:id', movieDeleteHandler);
 app.get('/getMovie/:id', specificMovieHandler);
 
 app.get('*', notFoundHandler);  
-app.use(errorHandler); 
-
- 
+app.use(errorHandler);  
 
  
 function Favourite(id, title, release_date, poster_path, overview) {
@@ -197,10 +80,10 @@ function Error(status, responseText) {
 let url = `https://api.themoviedb.org/3/trending/movie/day?api_key=${process.env.APIKEY}`; 
 
 function homePageHandler(req, res) {
- 
- 
- 
+  
+
   return res.status(200).send('welcome to home page');  
+}
 
 function favouritePageHandler(req, res) {
   return res.status(200).send('Welcome to the Favorite Page ');  
@@ -211,9 +94,8 @@ function trendingPageHandler(req, res) {
     .get(url)
     .then((dataApi) => {
       
-     
       let obj = dataApi.data.results.map((ele) => {
-        return new Favourite(ele.id, ele.title, ele.release_date, ele.poster_path, ele.overview);  
+        return new Favourite(ele.id, ele.title, ele.release_date, ele.poster_path, ele.overview); 
       });
       res.status(200).json(obj);
     })
@@ -225,7 +107,7 @@ function trendingPageHandler(req, res) {
 }
 
 function searchHandler(req, res) {
-   
+  
 
   let urls = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.APIKEY}&language=en-US&query=spiderman&page=1&include_adult=false`;
   axios
@@ -272,7 +154,7 @@ function genreHandler(req, res) {
  
 function addMovieHandler(req, res) {
   let movies = req.body;
-  
+   
   let sql = 'INSERT INTO favmovies(title,original_title,vote_count,poster_path,overview,release_date) VALUES ($1,$2,$3,$4,$5,$6) RETURNING *;';
   let obj = [
     movies.title || '',
@@ -295,7 +177,7 @@ function addMovieHandler(req, res) {
 }
  
 function getMoviesHandler(req, res) {
-   
+  // eslint-disable-next-line quotes
   let sql = `SELECT * FROM favmovies;`;
   client
     .query(sql)
@@ -324,13 +206,13 @@ function movieUpdateHandler(req, res) {
   client
     .query(sql, obj)
     .then((data) => {
-      res.status(200).json(data.rows); 
+      res.status(200).json(data.rows);  
     })
     .catch((err) => {
       errorHandler(err, req, res);
     });
 }
- 
+//Task14
 function movieDeleteHandler(req, res) {
   const id = req.params.id;
   let sql = `DELTE FROM favmovies WHERE id=$1;`;  
@@ -345,7 +227,7 @@ function movieDeleteHandler(req, res) {
       errorHandler(err, req, res);
     });
 }
- 
+//Task14
 function specificMovieHandler(req, res) {
   const id = req.params.id;
   let sql = `SELECT * FROM favmovies WHERE id=$1;`;
@@ -372,13 +254,13 @@ function errorHandler(err, req, res) {
   };
   res.status(500).send(errorr);
 }
- client.connect().then(() => {
+client.connect().then(() => {
   app.listen(PORT, () => {
     console.log(`listining to port ${PORT}`);
   });
 });
 
- 
+
 setTimeout(() => {
   console.log('inside the setTimeout');
-}, 0);  
+}, 0); 
